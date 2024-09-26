1bfa07ad774240ed13b175be093a139e34e62fb7
blackand18 <119662487+blackand18@users.noreply.github.com>
2024-09-26 09:12:23 +0700

    Perbaikan UI User & Supplier 26092024

--- Areas/MasterData/Controllers/UserActiveController.cs
+++ Areas/MasterData/Controllers/UserActiveController.cs
@@ -162,7 +162,7 @@ namespace PurchasingSystemApps.Areas.MasterData.Controllers
                     NamaUser = vm.FullName,
                     Email = vm.Email,
                     UserName = vm.Email,
-                    IsActive = vm.IsActive
+                    IsActive = true
                 };
 
                 var user = new UserActive
@@ -182,7 +182,7 @@ namespace PurchasingSystemApps.Areas.MasterData.Controllers
                     Handphone = vm.Handphone,
                     Email = vm.Email,
                     Foto = uniqueFileName,
-                    IsActive = vm.IsActive
+                    IsActive = true
                 };
 
                 var passTglLahir = vm.DateOfBirth.ToString("ddMMMyyyy");                
@@ -305,6 +305,7 @@ namespace PurchasingSystemApps.Areas.MasterData.Controllers
                             user.Address = viewModel.Address;
                             user.Handphone = viewModel.Handphone;
                             user.Email = viewModel.Email;
+                            user.IsActive = viewModel.IsActive;
 
                             if (viewModel.Foto != null)
                             {
